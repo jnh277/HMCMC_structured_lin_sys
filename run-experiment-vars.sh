b20eb44cd86181197cf7d8255119814e7cce2d3a
jnh277 <johannes.n.hendriks@gmail.com>
2019-11-25 16:26:36 +1100

    attempting to make qsub work

--- run-experiment-vars.sh
+++ run-experiment-vars.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-
 #PBS -l select=1:ncpus=1:mem=4GB
-#PBS -l walltime=10:00:00
+#PBS -l walltime=10:00:00           
 #PBS -l software=torch
 #PBS -k oe
 
 source /etc/profile.d/modules.sh
 module load pystan
-
+ 
 cd /home/jnh277/HMCMC_structured_lin_sys
+
+echo "Running"
 
 python single_param_uniform_noise.py
 
